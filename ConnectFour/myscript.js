1a6b76d8dec91f18ee9cafe8b2f8bb2bc81491be
RaunG <raung@icloud.com>
2018-10-19 11:00:17 -0500

    Update ConnectFour/myscript.js

--- ConnectFour/myscript.js
+++ ConnectFour/myscript.js
@@ -12,3 +12,40 @@ function reportWin(rowNum,colNum) {
   console.log(rowNum);
   console.log(colNum);
 }
+
+function changeColor(rowIndex,colIndex,color){
+  return table.eg(rowIndex).find('td').eg(colIndex).find('button').css('background-color',color);
+}
+
+function returnColor(rowIndex,colIndex){
+  return table.eg(rowIndex).find('td').eg(colIndex).find('button').css('background-color');
+}
+
+function checkBottom(colIndex){
+  var colorReport = returnColor(5,colIndex)
+  for (var row = 5; row > -1; row--) {
+    colorReport = returnColor(row,colIndex);
+    if (colorReport === 'rgb(128, 128, 128)') {
+      return row
+    }
+  }
+}
+
+function colorMatchCheck(one,two,three,four){
+  return (one === two && one === three && one === four && one !== 'rgb(128, 128, 128)')
+}
+
+// Check for Horizontal Wins
+function horizontalWinCheck() {
+  for(var row = 0; row < 6; row++) {
+    for (var col = 0; col < 4; col++) {
+      if (colorMatchCheck(returnColor(row,col), returnColor(row,col+1), returnColor(row,col+2), returnColor(row,col+3))) {
+        console.log('vertical');
+        reportWin(row,col);
+        return true;
+      }else {
+        continue;
+      }
+    }
+  }
+}
